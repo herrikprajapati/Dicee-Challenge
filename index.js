868763ac35c86633de1ef232bc8c6ca2a60b327b
Herrik Prajapati <herrik2706@gmail.com>
2020-07-27 16:27:09 -0500

    created random number for Dice (On Right) in Index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 var randomNumber1 = Math.random() * 6;
 randomNumber1 = Math.floor(randomNumber1) + 1;
 
+var randomNumber2 = Math.random() * 6;
+randomNumber2 = Math.floor(randomNumber2) + 1;
+
 if (randomNumber1 === 1){
 
     document.querySelectorAll("img")[0].setAttribute("src", "images/dice1.png")
@@ -25,5 +28,31 @@ if (randomNumber1 === 1){
 } else if (randomNumber1 === 6){
 
     document.querySelectorAll("img")[0].setAttribute("src", "images/dice6.png")
+
+}
+
+if (randomNumber2 === 1){
+
+    document.querySelectorAll("img")[1].setAttribute("src", "images/dice1.png")
+
+} else if (randomNumber2 === 2){
+
+    document.querySelectorAll("img")[1].setAttribute("src", "images/dice2.png")
+
+} else if (randomNumber2 === 3){
+
+    document.querySelectorAll("img")[1].setAttribute("src", "images/dice3.png")
+
+} else if (randomNumber2 === 4){
+
+    document.querySelectorAll("img")[1].setAttribute("src", "images/dice4.png")
+
+} else if (randomNumber2 === 5){
+
+    document.querySelectorAll("img")[1].setAttribute("src", "images/dice5.png")
+
+} else if (randomNumber2 === 6){
+
+    document.querySelectorAll("img")[1].setAttribute("src", "images/dice6.png")
 
 }
